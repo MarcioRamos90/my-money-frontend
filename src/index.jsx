3e39c94761f9d7ef02c8fc78dcfee1bfd811b5fd
marcioalvesramos90@hotmail.com <marcioalvesramos90@hotmail.com>
2018-07-21 12:18:01 -0300

    Melhorando acoes da inclusao de ciclo de pagamento

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import promise from "redux-promise";
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from "./main/app";
 import Reducers from "./main/reducer";
 
 const devTools =
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(promise)(createStore)(Reducers, devTools);
+const store = applyMiddleware(multi, thunk, promise)(createStore)(Reducers, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
